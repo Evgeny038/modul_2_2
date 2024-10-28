80ed6ccd961a743d82157b0111df81471d6bad25
Evgeny038 <ugienjohnn@gmail.com>
2024-10-28 10:53:52 +0800

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,19 @@
+first = 245
+second = 311
+third = 462
+if first == second or first == third or second == third:
+    print(2)
+elif first == second == third:
+    print(3)
+elif first != second != third:
+    print(0)
+
+first = 100
+second = 200
+third = 100
+if first == second or first == third or second == third:
+    print(2)
+elif first == second == third:
+    print(3)
+elif first != second != third:
+    print(0)
